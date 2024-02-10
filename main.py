8f438ab22181240e191cd244f241f7cc1053c895
serg_myst <serg_myst@mail.ru>
2024-02-10 22:51:43 +0300

    new project

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from get_users import get_users_list
 from excel import save_to_excel
+from gui import main
 
 if __name__ == '__main__':
-    result_list = get_users_list()
-    save_to_excel(result_list)
+    main()
+
+    # result_list = get_users_list()
+    # save_to_excel(result_list)
 
 # Получить результат выполнения функции из потока
 # https://sky.pro/media/poluchenie-vozvrashhaemogo-znacheniya-iz-potoka-v-python/
