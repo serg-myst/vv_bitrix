e3b282d1e8f11bbd20225c1e91a26467579efaeb
serg_myst <serg_myst@mail.ru>
2024-02-20 12:19:56 +0300

    some changes

--- get_params.py
+++ get_params.py
@@ -1,19 +1,21 @@
 class Params:
     def __init__(self, fields: list, params: dict):
+        if params:
+            self.__params = {}
         self.fields = fields
-        self.params = params
+        self.__params = params
 
     def add_select(self):
-        self.params = dict((f'select[{ind}]', value) for ind, value in enumerate(self.fields))
+        self.__params = dict((f'select[{ind}]', value) for ind, value in enumerate(self.fields))
 
     def add_filter(self, field: str, value: int | str):
-        self.params[f"filter[{field}]"] = value
+        self.__params[f"filter[{field}]"] = value
 
     def add_order(self, field: str, value: int | str):
-        self.params[f"order[{field}]"] = value
+        self.__params[f"order[{field}]"] = value
 
     def get_params(self):
-        return self.params
+        return self.__params
 
 
 if __name__ == '__main__':
